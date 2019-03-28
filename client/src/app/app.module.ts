1be88a99592329cb9fb01c77b4e3c50d040942c0
leahy089 <leahy089@morris.umn.edu>
2019-03-28 13:08:53 -0500

    Merge branch 'ProfileFront' of https://github.com/UMM-CSci-3601-S19/iteration-2-bananya-squad into ProfileFront
    # Conflicts:
    #	client/src/app/app.routes.ts

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,15 +4,20 @@ import {BrowserModule} from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http';
 import {AppComponent} from './app.component';
 import {HomeComponent} from './home/home.component';
-import {ProfileComponent} from "./profile/profile.component";
 import {Routing} from './app.routes';
 import {APP_BASE_HREF} from '@angular/common';
+
 import {RideComponent} from "./rides/ride.component";
 import {RideListComponent} from "./rides/ride-list.component";
 import {RideListService} from "./rides/ride-list.service";
 import {AddRideComponent} from "./rides/add-ride.component";
 import {EditRideComponent} from "./rides/edit-ride.component";
 import {DeleteRideComponent} from "./rides/delete-ride.component";
+
+
+import {UserListComponent} from "./users/user-list.component";
+import {UserListService} from "./users/user-list.service";
+
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 
@@ -36,10 +41,12 @@ import {CustomModule} from './custom.module';
     RideListComponent,
     AddRideComponent,
     EditRideComponent,
-    DeleteRideComponent
+    DeleteRideComponent,
+    UserListComponent,
   ],
   providers: [
     RideListService,
+    UserListService,
     {provide: APP_BASE_HREF, useValue: '/'},
   ],
   entryComponents: [
--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,14 +3,14 @@ import {ModuleWithProviders} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import {RideListComponent} from "./rides/ride-list.component";
-import {ProfileComponent} from "./profile/profile.component";
+import {UserListComponent} from "./users/user-list.component";
 
 
 // Route Configuration
 export const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'rides', component: RideListComponent},
-  {path: 'profile', component: ProfileComponent}
+  {path: 'user', component: UserListComponent},
 ];
 
 export const Routing: ModuleWithProviders = RouterModule.forRoot(routes);
