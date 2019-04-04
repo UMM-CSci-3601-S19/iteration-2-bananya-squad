24a60e7015dc1f4c2f7400320a74bdec68697493
leahy089 <leahy089@morris.umn.edu>
2019-04-04 04:19:39 -0500

    Write tests for checking if vehicle list service calls the right URLs Done By Utkarsh and Kaelan (And a lot fo merge files)

---
+++ client/src/app/vehicles/vehicle-list.component.spec.ts
@@ -0,0 +1,73 @@
+/*
+import {ComponentFixture} from "@angular/core/testing";
+import {VehicleListComponent} from "./vehicle-list.component";
+import {Observable} from "rxjs/Observable";
+import {Vehicle} from "./vehicle";
+
+
+
+describe('Ride list', () => {
+
+  let vehicleList: VehicleListComponent;
+  let fixture: ComponentFixture<VehicleListComponent>;
+
+  let rideListServiceStub: {
+    getVehicle: () => Observable<Vehicle[]>
+  };
+
+  beforeEach(()=> {
+    vehicleListServiceStub = {
+      getVehicle: () => Observable.of([
+        {
+          ownerId: '',
+          model: 'Ferrari',
+          color: 'Red',
+          engine: 'gas',
+          mpg: '65',
+          ecoFriendly: true
+        },
+        {
+          ownerId: '',
+          model: 'Gucci',
+          color: 'Black',
+          engine: 'naturalGas',
+          mpg: '69',
+          ecoFriendly: true
+        },
+        {
+          ownerId: '',
+          model: 'Boi',
+          color: 'Blue',
+          engine: 'electric',
+          mpg: '12',
+          ecoFriendly: false
+        },
+        {
+          ownerId: '',
+          model: 'Truck',
+          color: 'Green',
+          engine: 'gas',
+          mpg: '20',
+          ecoFriendly: false
+        },
+        {
+          ownerId: '',
+          model: 'Big Truck',
+          color: 'Dark Green',
+          engine: 'diesel',
+          mpg: '10',
+          ecoFriendly: false
+        },
+        {
+          ownerId: '',
+          model: 'Karshs Poop Wifi',
+          color: 'White',
+          engine: 'hybrid',
+          mpg: '1',
+          ecoFriendly: false
+        }
+      ])
+    };
+  })
+})
+*/
